759606e0880952f77741486f01daf0913b8e4bef
Joe Shepherd <sudo.jshepherd01@gmail.com>
2020-11-23 14:11:11 +0000

    Add saving, update readme

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Zoom Participants Monitor
-A simple tool for monitoring the number of participants in a zoom meeting.
+A simple ~~hack~~ tool for monitoring the number of participants in a zoom meeting.
 
 ## Usage
 - Join the meeting from your browser (not from the app)
@@ -9,3 +9,4 @@ A simple tool for monitoring the number of participants in a zoom meeting.
 - The console will output a list of numbers. If you see 'Copied to clipboard', carry on to the next step, otherwise you'll need to copy the list yourself.
 - Run display.py, and paste the string from the last step as prompted
 - A graph should appear on your screen showing the number of participants in the call against the time since you started monitoring
+- A file called output.csv will be created, if one doesn't already exist, containing the data entered. If one already exists, the new data will be added without overwriting anything. If an unrelated output.csv file exists, it won't be changed and the data will not be saved
--- display.py
+++ display.py
@@ -2,6 +2,8 @@ from datetime import datetime
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
+headerstring = "timestamp (s),participants"
+
 inp = input("Paste the output from retrieve.js : ")
 inp = inp.strip()
 inp = inp.split(",")
@@ -11,6 +13,24 @@ for i in range(0,len(inp),2):
     values.append([int(inp[i])//1000, int(inp[i+1])])
 
 values.sort(key=lambda x: x[0])
+
+try:
+    f = open("output.csv")
+    if f.read().startswith(headerstring):
+        f.close()
+        f = open("output.csv",'a')
+        print("Updating output.csv")
+        writebuff = "\n".join([""]+[",".join([datetime.fromtimestamp(i[0]).isoformat(' '), str(i[1])]) for i in values])
+        f.write(writebuff)
+        f.close()
+    else:
+        print("output.csv found but not valid, aborting save")
+except FileNotFoundError:
+    print("Creating output.csv")
+    f = open("output.csv",'w')
+    writebuff = "\n".join([headerstring]+[",".join([datetime.fromtimestamp(i[0]).isoformat(' '), str(i[1])]) for i in values])
+    f.write(writebuff)
+    f.close()
 
 dates = [datetime.fromtimestamp(values[0][0])]
 numbers = [values[0][1]]
